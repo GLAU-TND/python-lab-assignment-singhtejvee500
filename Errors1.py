45cc1ea4543df1a3a9f9531e3a73fd438e94978a
singhtejvee500 <54625616+singhtejvee500@users.noreply.github.com>
2019-11-30 16:37:20 +0530

    Add files via upload

---
+++ Errors1.py
@@ -0,0 +1,14 @@
+h = (1,3,5,6,9)
+try:
+    ##h.remove(9)
+    h = a+'hi'
+    ##h = float('hlo')
+
+except AttributeError as e:
+    print("AttributeError",e)
+except TypeError as e:
+    print("TypeError",e)
+except ValurError as e:
+    print("ValueError",e)
+finally:
+    print("process complete")
