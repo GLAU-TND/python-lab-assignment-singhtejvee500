fd4575347000611c02e9a5836d19b71dc03da3b2
singhtejvee500 <54625616+singhtejvee500@users.noreply.github.com>
2019-11-30 16:38:31 +0530

    Add files via upload

---
+++ Errors2.py
@@ -0,0 +1,154 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+230
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+230
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(50,30))
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 9, in xyz
+    raise MyException('Custom Exception Occurred')
+MyException: Custom Exception Occurred
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(input(a,b)))
+NameError: name 'a' is not defined
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(int(input(a,b))))
+NameError: name 'a' is not defined
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+4
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(map(int, input().split())))
+TypeError: xyz() missing 1 required positional argument: 'b'
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+4 500
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(map(int, input().split())))
+TypeError: xyz() missing 1 required positional argument: 'b'
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+5 500
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(map(int, input().split())))
+TypeError: xyz() missing 1 required positional argument: 'b'
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+5 600
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(map(int, input().split())))
+TypeError: xyz() missing 1 required positional argument: 'b'
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+5 666
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    a = int(input())
+ValueError: invalid literal for int() with base 10: '5 666'
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py5 6644
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    a = int(input())
+ValueError: invalid literal for int() with base 10: 'RESTART: C:\\Users\\lenovo\\AppData\\Local\\Programs\\Python\\Python37-32\\custom exception.py5 6644'
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+Traceback (most recent call last):
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 12, in <module>
+    print(xyz(50,20))
+  File "C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py", line 9, in xyz
+    raise MyException('Custom Exception Occurred')
+MyException: Custom Exception Occurred
+>>> 
+ RESTART: C:\Users\lenovo\AppData\Local\Programs\Python\Python37-32\custom exception.py 
+250
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py 
+AttributeError 'tuple' object has no attribute 'remove'
+process complete
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py 
+AttributeError 'tuple' object has no attribute 'remove'
+process complete
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py 
+process complete
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py", line 4, in <module>
+    h = a+'hi'
+NameError: name 'a' is not defined
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py", line 11, in <module>
+    except ValurError as e:
+NameError: name 'ValurError' is not defined
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py 
+process complete
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py", line 5, in <module>
+    h = float('hlo')
+ValueError: could not convert string to float: 'hlo'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py", line 11, in <module>
+    except ValurError as e:
+NameError: name 'ValurError' is not defined
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py 
+AttributeError 'tuple' object has no attribute 'remove'
+process complete
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py 
+process complete
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py", line 4, in <module>
+    h = a+'hi'
+NameError: name 'a' is not defined
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors.py", line 11, in <module>
+    except ValurError as e:
+NameError: name 'ValurError' is not defined
+>>> 
+ RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors1.py 
+process complete
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors1.py", line 5, in <module>
+    h = float('hlo')
+ValueError: could not convert string to float: 'hlo'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:/Users/lenovo/AppData/Local/Programs/Python/Python37-32/Errors1.py", line 11, in <module>
+    except ValurError as e:
+NameError: name 'ValurError' is not defined
+>>> 
